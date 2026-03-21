53300d2065a5fa156860588397206d79312b7002
Jurgen Xhihani <252644773+jxhihani24-cloud@users.noreply.github.com>
2026-03-21 12:47:53 +0100

    dark mode fahhh

--- script/main.js
+++ script/main.js
@@ -5,6 +5,10 @@ Typical things inside:
 menu button behavior
 dark/light interactions
 shared buttons
-general event listeners
+general event listeners*/
 
- */
+//set dark mode on page load
+document.addEventListener('DOMContentLoaded', () => {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    document.body.classList.add('dark-mode');
+});
