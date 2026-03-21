0e89c2e5c5ffe0e6beed4334fe99ac220ed7695c
Kevin <kevimuho@gmail.com>
2026-03-21 11:09:21 +0100

    Changes to html files and creating outline JS file (only comments)

---
+++ outline.js
@@ -0,0 +1,26 @@
+/*
+
+1) ==> Homepage (index.html)
+What it usually contains:
+
+Navigation bar- links to other pages like listings or login
+Search section- where users can search for places (city, dates, guests)
+Featured listings- a few highlighted properties
+Categories or locations- popular places to stay
+Footer- contact info, links, etc.
+
+Its role in the website:index.html acts as the central hub that directs users to the rest of the site.
+
+2) ==> Listings(listings.html):
+Shows all available properties (houses/apartments) that users can browse.
+Each property appears as a card with an image, title, location, and price, and users can click one to see more details.
+
+3) ==> Log In Page (login.html):
+Allows users to log into their account by entering their email/username and password.
+ After logging in, users can manage bookings or make reservations.
+
+4) ==> Property (property.html):
+Displays the full details of a single property. 
+This page includes photos, description, price per night, amenities, location, and a button to book or reserve the stay.
+
+     */
